8eb7cae25f8baf7405c97be8046cc21570d4214f
DESKTOP-CDF4VM5\albog <alboghardau@gmail.com>
2021-07-05 23:29:14 +0300

    sync

---
+++ MeridianAPI/Models/MicroService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeridianAPI.Models
+{
+    public class MicroService
+    {
+        public string Token { get; set; }
+        public string Name { get; set; }
+        public bool IsAlive { get; set; }
+
+    }
+}
