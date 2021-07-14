64f05301dec5497eb247b4fdf7d6e07bf5bb857b
Bogdan Hardau <alboghardau@gmail.com>
2021-07-15 00:03:21 +0300

    Add config manager

---
+++ MeridianAPI.Common/Configuration/ConfigManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeridianAPI.Common.Configuration
+{
+    class ConfigManager
+    {
+        private static readonly ConfigManager instance = new ConfigManager();
+
+        static ConfigManager() { }
+
+        private ConfigManager()
+        {
+
+        }
+    }
+}
