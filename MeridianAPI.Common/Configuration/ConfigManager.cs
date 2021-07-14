92b5575772fa544fb8566d0bd58dc0ca2799e2ab
Bogdan Hardau <alboghardau@gmail.com>
2021-07-15 00:18:39 +0300

    Config update

--- MeridianAPI.Common/Configuration/ConfigManager.cs
+++ MeridianAPI.Common/Configuration/ConfigManager.cs
@@ -1,20 +1,34 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MeridianAPI.Common.Configuration
 {
-    class ConfigManager
+    public class ConfigManager
     {
-        private static readonly ConfigManager instance = new ConfigManager();
+        JObject configuration;
 
+        #region Singleton
+        private static readonly ConfigManager instance = new ConfigManager();
         static ConfigManager() { }
 
         private ConfigManager()
         {
-
+            string path = Path.Combine(AppContext.BaseDirectory, "config.json");
+            configuration = JObject.Parse(File.ReadAllText(path));
         }
+
+        public static ConfigManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
     }
 }
