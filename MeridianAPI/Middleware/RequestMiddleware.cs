d18c192a3dceb0caddfc9cf7587d1a92d70b4c3c
Bogdan Hardau <alboghardau@gmail.com>
2021-08-03 01:03:35 +0300

    Add middlware

---
+++ MeridianAPI/Middleware/RequestMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeridianAPI.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RequestMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine("Middleware works.");
+            await httpContext.Response.WriteAsync("Test");
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class RequestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestMiddleware>();
+        }
+    }
+}
--- MeridianAPI/Startup.cs
+++ MeridianAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeridianAPI.Middleware;
 
 namespace MeridianAPI
 {
@@ -28,32 +30,37 @@ namespace MeridianAPI
         {
             services.AddHttpClient();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeridianAPI", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeridianAPI", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MeridianAPI v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    //app.UseDeveloperExceptionPage();
+            //    //app.UseSwagger();
+            //    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MeridianAPI v1"));
+            //}
+
+            app.UseRequestMiddleware();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-          
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+
+
         }
     }
 }
